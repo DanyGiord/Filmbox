654933e5501a1855ffa8a05d7b248502cf67436f
Daniel <daniel.giordani@marmelo.app>
2023-11-06 12:26:43 +0100

    added folders in UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import { Button } from "@/components/ui/buttons/button";
 import React from "react";
 
 function page() {
   return (
     <div>
-      <button className="card_third w-32 h-16">
-        <h1>caso</h1>
-      </button>
+      <Button>Watch Now</Button>
     </div>
   );
 }
